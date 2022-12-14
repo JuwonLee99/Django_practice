09d71031a841474e17f678120edae28ec7e5aed8
JuwonLee99 <joofuud98@gmail.com>
2022-12-14 12:50:29 +0900

    모델 추가 (makemigrations)

--- api_practice/pybo/models.py
+++ api_practice/pybo/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Question(models.Model):
-    subject = models.CharFteld(max_length=200)
+    subject = models.CharField(max_length=200)
     content = models.TextField()
-    create_date = models.DateTimField()
+    create_date = models.DateTimeField()
 
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
-    create_date = models.DateTimField()
+    create_date = models.DateTimeField()
 
